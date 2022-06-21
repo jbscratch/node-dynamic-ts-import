0a0ea9d5dd13e90c3a14d96bbc9c55f235bef3fe
Benjamin Jesuiter <benjamin.jesuiter.extern@atruvia.de>
2022-06-21 18:24:06 +0200

    compile ts file dynamically

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+import { create } from 'ts-node';
+import fs from 'fs-extra';
+
 async function main() {
 	console.log('Running');
+	// TS Config Docs: https://www.typescriptlang.org/tsconfig
+	const tsService = create({
+		compilerOptions: {
+			target: 'es2020',
+			lib: ['DOM'],
+			sourceMap: false,
+			inlineSourceMap: false,
+		},
+	});
+
+	const dynamicTsFile = await fs.readFile(`./my-ts-module.ts`, 'utf-8');
+	const jsOutput = tsService.compile(dynamicTsFile, './my-ts-module.ts');
+
+	console.log({ dynamicTsFile, jsOutput });
 }
 
 main();
